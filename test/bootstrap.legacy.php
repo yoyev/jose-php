2e185859bd0b2892f4cb672e19475f319ba05090
nov <nov.matake@gree.net>
2013-04-25 14:15:37 +0900

    include path fix

--- test/bootstrap.legacy.php
+++ test/bootstrap.legacy.php
@@ -2,7 +2,7 @@
 
 set_include_path(
     get_include_path() . PATH_SEPARATOR .
-    dirname(__FILE__) . '/../vendor/phpseclib/phpseclib'
+    dirname(__FILE__) . '/../vendor/phpseclib/phpseclib/phpseclib'
 );
 
 require_once dirname(__FILE__) . '/JOSE/TestCase.php';
